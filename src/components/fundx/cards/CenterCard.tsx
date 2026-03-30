826c11e5c59004cbdc7dc9ebcc5a95f28d645abb
jadonamite <jadonamite@gmail.com>
2026-03-30 15:38:14 +0100

    refactor: improve ui structure (improvement)

--- src/components/fundx/cards/CenterCard.tsx
+++ src/components/fundx/cards/CenterCard.tsx
@@ -1,6 +1,6 @@
-import { Campaign } from "@/lib/data"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
+import { Campaign } from "@/lib/data"
 import { Heart } from "lucide-react"
 
 interface CenterCardProps {
