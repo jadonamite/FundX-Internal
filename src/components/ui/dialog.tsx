e72bca1bcdca2c75c2593bb1fd0287c7cba3bbeb
jadonamite <jadonamite@gmail.com>
2026-03-31 15:52:41 +0100

    refactor: simplify ui logic (improvement)

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { XIcon } from "lucide-react"
 import * as React from "react"
-import { cn } from "@/lib/utils"
-
-import { Button } from "@/components/ui/button"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
+import { XIcon } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
 
 function Dialog({
   ...props
