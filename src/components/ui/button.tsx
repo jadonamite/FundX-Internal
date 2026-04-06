cd2ef9baac1af472a6ba254d32509ab7c5d891e5
jadonamite <jadonamite@gmail.com>
2026-04-06 14:15:39 +0100

    fix: fix issue in ui (improvement)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -43,7 +43,7 @@ function Button({
   variant = "default",
   size = "default",
   asChild = false,
-  ...props
+  ...props_
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
@@ -56,7 +56,7 @@ function Button({
       data-variant={variant}
       data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
+      {...props_}
     />
   )
 }
