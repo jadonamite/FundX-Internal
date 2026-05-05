2e15e4ff8d019f2cff397374b668846ac3fabe56
jadonamite <jadonamite@gmail.com>
2026-05-05 01:34:04 +0100

    fix: fix issue in ui & logic (quick fix)

--- src/components/create/WizardSteps.tsx
+++ src/components/create/WizardSteps.tsx
@@ -2,10 +2,10 @@
 
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { CreateCampaignData } from "@/app/create/page" 
-import { Rocket } from "lucide-react"
 import { Label } from "@/components/ui/label"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Rocket } from "lucide-react"
+import { CreateCampaignData } from "@/app/create/page" 
 
 
 interface WizardProps {
