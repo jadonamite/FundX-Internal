03f6460aa8401d20db3af943faf48efc06570487
jadonamite <jadonamite@gmail.com>
2026-05-10 18:12:18 +0100

    fix: fix issue in ui (cleanup)

--- src/components/fundx/ConnectWallet.tsx
+++ src/components/fundx/ConnectWallet.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import { useEffect, useState } from "react"
+import { 
 import { Button } from "@/components/ui/button"
 import { useStacks } from "@/components/fundx/StacksProvider"
-import { 
   DropdownMenu, 
   DropdownMenuContent, 
   DropdownMenuItem, 
@@ -11,8 +11,8 @@ import {
   DropdownMenuSeparator, 
   DropdownMenuTrigger 
 } from "@/components/ui/dropdown-menu"
-import { ChevronDown, Copy, LogOut, Wallet } from "lucide-react"
 import { toast } from "sonner" // <--- Import Toast
+import { ChevronDown, Copy, LogOut, Wallet } from "lucide-react"
 
 export function ConnectWallet() {
   const { authenticate, signOut, isSignedIn, walletData } = useStacks()
