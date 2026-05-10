f33980b987a28383eac73696ae913e6387433bf2
jadonamite <jadonamite@gmail.com>
2026-05-10 18:42:04 +0100

    cleanup: clean up ui (cleanup)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import { StacksProvider } from "@/components_/fundx/StacksProvider";
-import { Toaster } from "@/components_/ui/sonner";
+import { StacksProvider } from "@/components/fundx/StacksProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"], variable: "--font-jakarta" });
 
