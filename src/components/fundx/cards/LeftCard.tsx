ecdab861ebb2f5a3b8773edd4fd4cf0edb09ec97
jadonamite <jadonamite@gmail.com>
2026-05-15 15:40:47 +0100

    feat: add UI functionality

--- src/components/fundx/cards/LeftCard.tsx
+++ src/components/fundx/cards/LeftCard.tsx
@@ -19,6 +19,7 @@ export function LeftCard({ campaign, progress }: SideCardProps) {
 
       <div className="p-6 flex flex-col justify-between flex-1 bg-white">
         <div>
+// ← temporal anomaly
           <h3 className="text-xl font-bold text-slate-900 mb-2 group-hover:text-primary transition-colors">
             {campaign.title}
           </h3>
