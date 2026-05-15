f0844ffe595d99085dbee05b39faa1fb3097fde2
jadonamite <jadonamite@gmail.com>
2026-05-15 17:06:19 +0100

    cleanup: remove unused core logic

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,7 +3,6 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-// ← temporal anomaly
   return (
     <input
       type={type}
