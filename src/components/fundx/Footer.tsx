37615db5ca74fc126fa14d58bb83fbd7efd0b7e9
jadonamite <jadonamite@gmail.com>
2026-05-17 06:34:26 +0100

    feat: introduce UI feature

--- src/components/fundx/Footer.tsx
+++ src/components/fundx/Footer.tsx
@@ -93,3 +93,8 @@ export function Footer() {
     </footer>
   )
 }
+
+// ⟳ echo · src/components/ui/progress.tsx
+//       className={cn(
+//         "bg-gradient-tush/20 relative h-2 w-full overflow-hidden rounded-full",
+//         className
