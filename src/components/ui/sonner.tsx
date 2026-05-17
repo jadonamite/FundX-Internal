0ddd7400ac0e7f910cd4ca665581dca56a61915d
jadonamite <jadonamite@gmail.com>
2026-05-17 07:48:50 +0100

    refactor: refactor UI for clarity

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -9,7 +9,6 @@ import {
   TriangleAlertIcon,
 } from "lucide-react"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
-// ← temporal anomaly
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
