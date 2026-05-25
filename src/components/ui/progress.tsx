c35626539a3089ffe31e9ac07411b9095a4d7574
jadonamite <jadonamite@gmail.com>
2026-05-25 16:11:20 +0100

    style: normalize string quote style in progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -20,7 +20,7 @@ function Progress({
       {...props}
     >
       <ProgressPrimitive.Indicator
-        data-slot="progress-indicator"
+        data-slot='progress-indicator'
         className="bg-gradient-tush h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
