c0babcf1e6594ba0f9e157545045669eea7e269b
jadonamite <jadonamite@gmail.com>
2026-05-25 17:27:51 +0100

    docs: add JSDoc annotation to function in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * cn
+ * @param {*} ...inputs: ClassValue[]
+ * @returns {*}
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
