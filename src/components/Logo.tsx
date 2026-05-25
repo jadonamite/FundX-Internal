5564b13f17104b00a535afd031af73b378db4997
jadonamite <jadonamite@gmail.com>
2026-05-25 13:39:02 +0100

    docs: add JSDoc annotation to function in Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
+/**
+ * Logo
+ * @param {*} { className }: { className?: string }
+ * @returns {*}
+ */
 export default function Logo({ className }: { className?: string }) {
   return (
     <Image
