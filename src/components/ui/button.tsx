06e8b4ff30945ab6dc68a70656d535e975c83ee7
jadonamite <jadonamite@gmail.com>
2026-05-25 16:21:05 +0100

    style: normalize string quote style in button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -27,7 +27,7 @@ const buttonVariants = cva(
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
         "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
-        "icon-sm": "size-8",
+        'icon-sm': 'size-8',
         "icon-lg": "size-10",
       },
     },
