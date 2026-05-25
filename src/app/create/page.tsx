2ad6a758c015a402731f3d98601683922df79d36
jadonamite <jadonamite@gmail.com>
2026-05-25 17:15:06 +0100

    docs: add JSDoc annotation to function in page.tsx

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -33,6 +33,10 @@ export interface CreateCampaignData {
   currency: "USDCx" | "STX";
 }
 
+/**
+ * CreateCampaign
+ * @returns {*}
+ */
 export default function CreateCampaign() {
   const { isSignedIn, authenticate } = useStacks()
   const [step, setStep] = useState(1)
