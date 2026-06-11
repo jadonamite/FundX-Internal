f96d3f5364f3f226ff48cb69b47d932e612732d4
jadonamite <jadonamite@gmail.com>
2026-06-11 21:34:56 +0100

    style: add trailing commas for consistency in WizardSteps.tsx

--- src/components/create/WizardSteps.tsx
+++ src/components/create/WizardSteps.tsx
@@ -11,7 +11,7 @@ import { CreateCampaignData } from "@/app/create/page"
 interface WizardProps {
   step: number
   formData: CreateCampaignData
-  setFormData: (data: CreateCampaignData) => void
+  setFormData: (data: CreateCampaignData) => void,
 }
 
 function FieldError({ msg }: { msg: string }) {
