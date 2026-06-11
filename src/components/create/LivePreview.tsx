d8b87f01a30f7e995662b1667652a1b6d95ad4cd
jadonamite <jadonamite@gmail.com>
2026-06-11 21:22:56 +0100

    docs: add JSDoc annotation to function in LivePreview.tsx

--- src/components/create/LivePreview.tsx
+++ src/components/create/LivePreview.tsx
@@ -1,5 +1,10 @@
 import { CampaignCard } from "@/components/fundx/CampaignCard"
 
+/**
+ * LivePreview
+ * @param {*} { formData }: { formData: any }
+ * @returns {*}
+ */
 export function LivePreview({ formData }: { formData: any }) {
   // Check if we are using STX to change colors
   const isSTX = formData.currency === "STX";
