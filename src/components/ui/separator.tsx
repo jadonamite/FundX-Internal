5084cac460debe0e9e44e11c70c3a18f87298ce9
jadonamite <jadonamite@gmail.com>
2026-06-18 15:21:22 +0100

    fix(ui): remove unused import and simplify className logic.

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,28 +1,26 @@
-"use client"
-
+use client
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
 import { cn } from "@/lib/utils"
 
-function Separator({
-  className,
-  orientation = "horizontal",
-  decorative = true,
-  ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+const getSeparatorClassName = (orientation: 'horizontal' | 'vertical', className?: string) => {
+  const baseClassName = [
+    "bg-border",
+    "shrink-0",
+    orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
+  ].join(' ')
+  return cn(baseClassName, className)
+}
+
+function Separator({ className, orientation = "horizontal", decorative = true, ...props }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
-      )}
+      className={getSeparatorClassName(orientation, className)}
       {...props}
     />
   )
 }
-
 export { Separator }
