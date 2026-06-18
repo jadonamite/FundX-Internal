a5eafc94c5e30bb970a9046c76c0a5647085f30f
jadonamite <jadonamite@gmail.com>
2026-06-18 16:33:54 +0100

    feat(ui): update avatar and label components to use client.

--- src/components/fundx/cards/MobileCard.tsx
+++ src/components/fundx/cards/MobileCard.tsx
@@ -8,6 +8,12 @@ interface MobileCardProps {
   progress: number
 }
 
+/**
+ * MobileCard
+ * @param {*} { campaign
+ * @param {*} progress }: MobileCardProps
+ * @returns {*}
+ */
 export function MobileCard({ campaign, progress }: MobileCardProps) {
   return (
     <div className="w-full bg-white rounded-[2rem] shadow-soft-md border border-slate-100 overflow-hidden flex flex-col">
