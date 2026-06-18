8670aca34056b6422c1f53f125b63bee40d45e24
jadonamite <jadonamite@gmail.com>
2026-06-18 15:23:14 +0100

    refactor(ui): refactor label, separator

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,26 +1,29 @@
-use client
+"use client"
+
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
 import { cn } from "@/lib/utils"
 
-const getSeparatorClassName = (orientation: 'horizontal' | 'vertical', className?: string) => {
-  const baseClassName = [
-    "bg-border",
-    "shrink-0",
-    orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
-  ].join(' ')
-  return cn(baseClassName, className)
-}
-
-function Separator({ className, orientation = "horizontal", decorative = true, ...props }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator"
       decorative={decorative}
+      // NOTE: revisit this logic after API migration
       orientation={orientation}
-      className={getSeparatorClassName(orientation, className)}
+      className={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className
+      )}
       {...props}
     />
   )
 }
+
 export { Separator }
