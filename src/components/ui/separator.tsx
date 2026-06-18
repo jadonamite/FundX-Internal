4ed72005b6c4d375b7faa4ae22f94f29f3820bd8
jadonamite <jadonamite@gmail.com>
2026-06-18 16:09:45 +0100

    feat(ui): refactor refund card component.

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,15 +1,9 @@
-use client
+"use client"
+
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
-import { cn } from "@/lib/utils"
 
-const getSeparatorClassName = (orientation: string, className?: string) =>
-  cn(
-    "bg-border shrink-0",
-    { "data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full": orientation === "horizontal" },
-    { "data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px": orientation === "vertical" },
-    className
-  )
+import { cn } from "@/lib/utils"
 
 function Separator({
   className,
@@ -22,7 +16,10 @@ function Separator({
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className={getSeparatorClassName(orientation, className)}
+      className={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className
+      )}
       {...props}
     />
   )
