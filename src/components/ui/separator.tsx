1a263af81f72516c0a04f1e888176cb2b7bf4ca7
jadonamite <jadonamite@gmail.com>
2026-06-18 16:41:07 +0100

    feat(ui): add getSeparatorClassName utility function

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,9 +1,16 @@
-"use client"
-
+use client
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
 import { cn } from "@/lib/utils"
+
+function getSeparatorClassName(orientation: string, className?: string) {
+  return cn(
+    "bg-border shrink-0",
+    { "data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full": orientation === "horizontal" },
+    { "data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px": orientation === "vertical" },
+    className
+  )
+}
 
 function Separator({
   className,
@@ -16,13 +23,9 @@ function Separator({
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
-      )}
+      className={getSeparatorClassName(orientation, className)}
       {...props}
     />
   )
 }
-
 export { Separator }
