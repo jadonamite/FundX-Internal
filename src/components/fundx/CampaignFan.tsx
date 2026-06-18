cd7cb850c28f5476e8152671d59b215f615dc04a
jadonamite <jadonamite@gmail.com>
2026-06-18 16:57:59 +0100

    fix(fundx): fix CampaignFan, CenterCard, index

--- src/components/fundx/CampaignFan.tsx
+++ src/components/fundx/CampaignFan.tsx
@@ -109,10 +109,6 @@ export function CampaignFan({ deckSlotRef }: CampaignFanProps) {
     Math.min((raised / goal) * 100, 100)
 
   useEffect(() => {
-    /**
-     * measure
-     * @returns {*}
-     */
     function measure() {
       if (!deckSlotRef.current || !deckRef.current) return
       const slotPageTop = getPageOffsetTop(deckSlotRef.current)
