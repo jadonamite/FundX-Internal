ab8724046f18063a55e406de5d52130772113476
jadonamite <jadonamite@gmail.com>
2026-06-19 17:09:37 +0100

    docs(ui): remove unused import statement.

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,15 +1,9 @@
-use client
+"use client"
+
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
-import { cn } from "@/lib/utils"
 
-const getSeparatorClassName = (orientation: 'horizontal' | 'vertical', className?: string) => {
-  const baseClassName = "bg-border shrink-0"
-  const orientationClassName = orientation === 'horizontal'
-    ? "data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full"
-    : "data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px"
-  return cn(baseClassName, orientationClassName, className)
-}
+import { cn } from "@/lib/utils"
 
 function Separator({
   className,
@@ -22,7 +16,10 @@ function Separator({
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className={getSeparatorClassName(orientation, className)}
+      className={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        className
+      )}
       {...props}
     />
   )
