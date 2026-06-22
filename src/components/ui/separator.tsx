64591be114f32d4ba83fb5ebb6273c8348cc41a5
jadonamite <jadonamite@gmail.com>
2026-06-22 20:17:22 +0100

    fix(components): refactor refund request handling in BackerTab component

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,9 +1,16 @@
-"use client"
-
+use client
 import * as React from "react"
 import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
 import { cn } from "@/lib/utils"
+
+const getSeparatorClassName = (orientation: 'horizontal' | 'vertical', className?: string) => {
+  const baseClassName = [
+    "bg-border",
+    "shrink-0",
+    orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
+  ].join(' ')
+  return cn(baseClassName, className)
+}
 
 function Separator({
   className,
@@ -16,10 +23,7 @@ function Separator({
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
-      )}
+      className={getSeparatorClassName(orientation, className)}
       {...props}
     />
   )
