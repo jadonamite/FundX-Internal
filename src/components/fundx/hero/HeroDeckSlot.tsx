91bd9f234d470e97cfb5c9170a955fae77ae0715
jadonamite <jadonamite@gmail.com>
2026-06-24 02:16:25 +0100

    feat(ui): refactor progress component with new transform function

--- src/components/fundx/hero/HeroDeckSlot.tsx
+++ src/components/fundx/hero/HeroDeckSlot.tsx
@@ -1,12 +1,17 @@
-import React from "react"
+import React from "react";
+
+const HERO_DECK_SLOT_STYLES = {
+  height: "6rem",
+  pointerEvents: "none",
+};
 
 export function HeroDeckSlot({ slotRef }: { slotRef: React.RefObject<HTMLDivElement | null> }) {
   return (
     <div
       ref={slotRef}
       className="w-full flex justify-center items-center my-6"
-      style={{ height: "6rem", pointerEvents: "none" }}
+      style={HERO_DECK_SLOT_STYLES}
       aria-hidden="true"
     />
-  )
+  );
 }
