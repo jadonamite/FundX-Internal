4b2bc99b2748ea98b5a716ece68f11ce9ae3ba21
jadonamite <jadonamite@gmail.com>
2026-07-03 01:58:59 +0100

    feat(src/components): remove unused Logo component props.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,4 @@
 import Image from "next/image";
 
-export default function Logo({ className }: { className?: string }) {
-  return (
-    <Image
-      src="/Logo.svg"
-      alt="FundX Logo"
-      width={32}
-      height={16}
-      className={className}
-    />
-  );
-}
+const LOGO_PROPS = {
+  src: \
