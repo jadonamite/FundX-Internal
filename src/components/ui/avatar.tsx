5670a60b285ab2febdd516608904ed87e632564c
jadonamite <jadonamite@gmail.com>
2026-07-05 00:28:20 +0100

    fix(fundx): fix page, Footer, RightCard +4 more

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -5,7 +5,6 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
 
-// NOTE: revisit this logic after API migration
 function Avatar({
   className,
   size = "default",
