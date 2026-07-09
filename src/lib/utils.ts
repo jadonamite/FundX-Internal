cfa5e462192ade618e11ec2009c32ae2fbee98fd
jadonamite <jadonamite@gmail.com>
2026-07-09 05:38:26 +0100

    feat(utils): add cn function with support for multiple class values.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,11 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { HIRO_API } from "./stacks-config"
 
+/**
+ * cn
+ * @param {*} ...inputs: ClassValue[]
+ * @returns {*}
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
